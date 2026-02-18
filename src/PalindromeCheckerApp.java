716748bf882a177abfe9f7c0e797615d5b26dcac
vignesh-reddy-c <ga6626192@gmail.com>
2026-02-18 15:04:50 +0530

    UC3: user input polindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application - UC1");
+
+        System.out.println("Welcome to Palindrome Checker Application");
+
+        Scanner sc = new Scanner(system.in);
+        system.out.println("enter a word to check polindrome :");
+        String word = sc.nextLine();
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
     }
 }
