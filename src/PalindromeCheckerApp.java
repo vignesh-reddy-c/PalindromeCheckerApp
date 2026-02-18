a2221c990a38269363ed289c7b4ac1aea6800eef
vignesh-reddy-c <ga6626192@gmail.com>
2026-02-18 14:37:57 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application");
+        System.out.println("Welcome to Palindrome Checker Application - UC1");
     }
 }
