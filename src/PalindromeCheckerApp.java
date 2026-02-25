8911d31a31779c395f265e84a1a13970582a25d9
vignesh-reddy-c <ga6626192@gmail.com>
2026-02-25 15:36:50 +0530

    UC11: Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,31 @@
+package src;
 import java.util.Scanner;
-class PalindromeCheckerApp {
+import java.util.Stack;
+class PalindromeChecker {
+    public boolean checkPalindrome(String input) {
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < normalized.length(); i++) {
+            stack.push(normalized.charAt(i));
+        }
+        for (int i = 0; i < normalized.length(); i++) {
+            if (normalized.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        if (isPalindrome(normalized)) {
+        PalindromeChecker checker = new PalindromeChecker();
+        if (checker.checkPalindrome(input)) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is not a Palindrome.");
         }
-    }
-    static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
 }
